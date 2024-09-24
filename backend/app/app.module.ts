d6132e37e06c51b35e63c01bc6dc44e296f688a4
marco0antonio0 <marcomesquitajr@hotmail.com>
2024-09-24 01:18:01 -0300

    up

--- backend/app/app.module.ts
+++ backend/app/app.module.ts
@@ -8,7 +8,7 @@ import sequelize from '../config/sequelize.config';
 export const AppModule = (app: Application) => {
     app.use(express.json());
     // Sincronização do Sequelize (cria tabelas se elas não existirem)
-    sequelize.sync({ force: true }) // force: true recria as tabelas toda vez que reiniciar
+    sequelize.sync({ force: false }) // force: true recria as tabelas toda vez que reiniciar
 
     // API de exemplo
     app.use('/api', pacienteController);
